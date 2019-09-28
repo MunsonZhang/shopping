233170126c4a3011f70725f7ef644603b7b4a454
MunsonZhang <978068205@qq.com>
2019-09-28 23:06:19 +0800

    实体类

---
+++ shopping/src/test/java/com/munson/shopping/user/entity/User.java
@@ -0,0 +1,14 @@
+package com.munson.shopping.user.entity;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name="tbUser")
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
